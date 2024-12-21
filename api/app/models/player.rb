46d4e6ca526299d79807603b76e14a5405b3f11f
dev-cruz <joaovictorcruz82@gmail.com>
2024-12-21 19:25:49 -0300

    feat: added Player entity

--- api/app/models/player.rb
+++ api/app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+  belongs_to :rooms, optional: true
+  validates :name, presence: true
 end
--- api/db/migrate/20241221190225_create_players.rb
+++ api/db/migrate/20241221190225_create_players.rb
@@ -2,7 +2,9 @@ class CreatePlayers < ActiveRecord::Migration[8.0]
   def change
     create_table :players do |t|
       t.string :name
-      t.integer :chips
+      t.integer :chips, default: 1000
+      t.string :cards, array: true, default: []
+      t.references :room, null: true, foreign_key: true
 
       t.timestamps
     end
