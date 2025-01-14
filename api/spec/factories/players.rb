8695d550a93c3cf3600cbf7526a5f4d2294b3e99
dev-cruz <joaovictorcruz82@gmail.com>
2025-01-14 01:49:23 -0300

    chore: setup ruby tests

---
+++ api/spec/factories/players.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :player do
+    name { "Jhon Doe" }
+    chips { 1000 }
+  end
+end
--- api/test/controllers/players_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PlayersControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get players_create_url
-    assert_response :success
-  end
-end
